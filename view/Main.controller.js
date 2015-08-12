03499a93e2227a6ff6837b437e525fa9ee8a9c43
kicker123 <chriskne+00@gmail.com>
2015-08-12 09:46:14 +0000

    initial commit

---
+++ view/Main.controller.js
@@ -0,0 +1,16 @@
+sap.ui.controller("sap.training.exc08.property_binding.view.Main", {
+
+	onInit: function() {
+
+		var oData = {
+		    name: "Peter",
+			enabled: true
+		};
+		
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.setData(oData);
+
+		this.getView().setModel(oModel);
+	}
+
+});
