3b5e57038dfdcdabe5a810e1ff79425e757e9344
kicker123 <chriskne+00@gmail.com>
2015-08-12 12:53:07 +0000

    JSON Model corrected / content Aggregation added to VerticalLayout

--- view/Main.controller.js
+++ view/Main.controller.js
@@ -3,8 +3,8 @@ sap.ui.controller("sap.training.exc08.property_binding.view.Main", {
 	onInit: function() {
 
 		var oData = {
-		    name: "Peter",
-			enabled: true
+		    "name": "Peter",
+			"enabled": true
 		};
 		
 		var oModel = new sap.ui.model.json.JSONModel();
